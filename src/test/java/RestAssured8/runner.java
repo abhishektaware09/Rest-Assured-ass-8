08ba2f849ee3b5239a29cc03076a342e1f66ae3c
abhishektaware09 <abhitaware91@gmail.com>
2020-11-03 00:36:47 +0530

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

---
+++ src/test/java/RestAssured8/runner.java
@@ -0,0 +1,12 @@
+package RestAssured8;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(strict = true)
+@RunWith(Cucumber.class)
+public class runner extends testdefination{
+
+}
